ca47a86df15a83cddb854aefdbffbeaeb93b2c46
gitansal <ansales1234@gmail.com>
2023-02-24 07:38:59 +0530

    change

--- models/appointmentModels.go
+++ models/appointmentModels.go
@@ -14,8 +14,8 @@ type Appointment struct {
 	Address    *string            `json:"address" validate:"required"`
 	Purpose    *string            `json:"purpose" validate:"required"`
 	User_id    *string            `json:"user_id" validate:"required"`
-	Date       *string            `json:"appointment_date" validate:"required"`
-	Time       *string            `json:"appointment_time" validate:"required"`
+	Date       *string            `json:"date" validate:"required"`
+	Time       *string            `json:"time" validate:"required"`
 	Created_at time.Time          `json:"created_at"`
 	Updated_at time.Time          `json:"updated_at"`
 }
